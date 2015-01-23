2b18d01660da3f0c67b3de534c51d9554820553b
ankitpraddhan <pradhan922@gmail.com>
2015-01-22 18:05:19 -0800

    Animal Pattern file added.
    It is written in java.

---
+++ Animal_pattern/src/Animal_pattern.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Nino on 1/19/2015.
+ */
+public class Animal_pattern {
+    public static void main(String[] args) {
+        System.out.println("           '    '" );
+        System.out.println ("            ____");
+        System.out.println ("           ( \u00A9\u00A9 )" );
+        System.out.println ("  /==========\\/");
+        System.out.println (" /  ||  %%  ||");
+        System.out.println ("*   ||------||");
+        System.out.println ("    \u00A5\u00A5      \u00A5\u00A5");
+        System.out.println ("    \"\"      \"\"");
+        System.out.println ("");
+
+    }
+}
